ca9fbec76be3ce899a8ccb72fe8165046bba79db
Marcus <33243769+marcuschui1@users.noreply.github.com>
2019-05-06 22:49:48 +0800

    Add files via upload

---
+++ precache-manifest.a59190955f007cd8f5727b648f0ece27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd0c9495c443fa57c965776e03a2b17",
+    "url": "/facedetech/index.html"
+  },
+  {
+    "revision": "d74b66972edb31da520a",
+    "url": "/facedetech/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a8339c1760a36ea4cba8",
+    "url": "/facedetech/static/css/main.dc4e0a5b.chunk.css"
+  },
+  {
+    "revision": "d74b66972edb31da520a",
+    "url": "/facedetech/static/js/2.8469c0be.chunk.js"
+  },
+  {
+    "revision": "a8339c1760a36ea4cba8",
+    "url": "/facedetech/static/js/main.c7390da4.chunk.js"
+  },
+  {
+    "revision": "6da8eb563269f4ec7fa6",
+    "url": "/facedetech/static/js/runtime~main.0ce34508.js"
+  }
+]);
